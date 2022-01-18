ea7e9620f83a2c3e54291a99d644eecc66079a03
Madu Valentine <maduvalentine8@gmail.com>
2022-01-18 13:14:03 +0100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,7 +2,7 @@
 
 
 cp -r ./geostart ./simfix
-cd  simfix/geostart/src
+cd  simfix/src
 mvn clean install -Dovrdir=postgres -Ppostgres
 cd web/app
 mvn jetty:run -Dovrdir=postgres -Pextjs,postgres &
